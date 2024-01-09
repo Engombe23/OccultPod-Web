09a54bde0121372002a2a13a87ff9c9f6230506b
Engombe Lokanga <engombeel13@gmail.com>
2024-01-09 11:27:25 +0000

    Navigation

--- frontend/src/components/Navigate.js
+++ frontend/src/components/Navigate.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import {Container, Nav, Navbar} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import {SocialIcon} from 'react-social-icons';
+import {Nav, Navbar} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 function Navigate() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand as={Link} to={"/"}>OccultPod</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary justify-content-center" fixed='top'>
+        <Nav className="justify-content-center" activeKey="/home">
+          <Nav.Item>
             <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
             <Nav.Link as={Link} to={"/about"}>About</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
             <Nav.Link as={Link} to={"/episodes"}>Episodes</Nav.Link>
-            <Nav.Link as={Link} to={"/listen"}>Where To Listen</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
             <Nav.Link as={Link} to={"/contact"}>Contact</Nav.Link>
-            <SocialIcon url='https://twitter.com/OccultPod_13'/>
-            <SocialIcon url='https://instagram.com/occultpod_13'/>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          </Nav.Item>
+        </Nav>
+      </Navbar>
+    </>
   )
 }
 
